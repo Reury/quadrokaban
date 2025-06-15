6379d786d7cc5f24fb86c54b28065293d8903726
Reury <reuryaraujo@gmail.com>
2025-06-14 22:48:15 -0300

    fix corrigido o import

--- src/test/java/com/reury/kabanquadro/service/BoardServiceTest.java
+++ src/test/java/com/reury/kabanquadro/service/BoardServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.reury.kabanquadro.model.TipoColuna;
 
 @SpringBootTest
 class BoardServiceTest {
