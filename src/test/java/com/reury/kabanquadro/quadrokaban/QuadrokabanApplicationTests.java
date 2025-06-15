534b76d760b5de64b884f9a1153909ceb4f744c0
Reury <reuryaraujo@gmail.com>
2025-06-14 22:59:13 -0300

    fix forcando o profile ci nos testes

--- src/test/java/com/reury/kabanquadro/quadrokaban/QuadrokabanApplicationTests.java
+++ src/test/java/com/reury/kabanquadro/quadrokaban/QuadrokabanApplicationTests.java
@@ -2,8 +2,10 @@ package com.reury.kabanquadro.quadrokaban;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("ci") // ADICIONE ESTA LINHA
 class QuadrokabanApplicationTests {
 
 	@Test
--- src/test/java/com/reury/kabanquadro/service/BoardServiceTest.java
+++ src/test/java/com/reury/kabanquadro/service/BoardServiceTest.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import com.reury.kabanquadro.model.TipoColuna;
 
 @SpringBootTest
+@ActiveProfiles("ci") // ADICIONE ESTA LINHA
 class BoardServiceTest {
 
     @Autowired
